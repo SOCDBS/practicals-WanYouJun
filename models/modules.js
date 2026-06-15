cba19f9fc329019e8e4df1efbd8ee13fabeb0a19
Wan You Jun <YOUJUN08.25@ichat.sp.edu.sg>
2026-06-15 18:30:44 +0800

    Delete by Module Code

--- models/modules.js
+++ models/modules.js
@@ -37,21 +37,31 @@ module.exports.retrieveByCode = function retrieveByCode(code) {
     });
 };
 
-module.exports.deleteByCode = function deleteByCode(code) {
-    // Note:
-    // If using raw sql: Can use result.rowCount to check the number of rows affected
-    // But if using function/stored procedure, result.rowCount will always return null
-    const sql = `DELETE FROM module WHERE mod_code = $1`;
-    return query(sql, [code]).then(function (result) {
-        const rows = result.rowCount;
+// module.exports.deleteByCode = function deleteByCode(code) {
+//     // Note:
+//     // If using raw sql: Can use result.rowCount to check the number of rows affected
+//     // But if using function/stored procedure, result.rowCount will always return null
+//     const sql = `DELETE FROM module WHERE mod_code = $1`;
+//     return query(sql, [code]).then(function (result) {
+//         const rows = result.rowCount;
 
-        if (rows === 0) {
-            // Note: result.rowCount returns the number of rows processed instead of returned
-            // Read more: https://node-postgres.com/apis/result#resultrowcount-int--null
-            throw new EMPTY_RESULT_ERROR(`Module ${code} not found!`);
-        }
-    })
-};
+//         if (rows === 0) {
+//             // Note: result.rowCount returns the number of rows processed instead of returned
+//             // Read more: https://node-postgres.com/apis/result#resultrowcount-int--null
+//             throw new EMPTY_RESULT_ERROR(`Module ${code} not found!`);
+//         }
+//     })
+// };
+
+module.exports.deleteByCode = function deleteByCode(code) {
+    return query('CALL delete_module($1)', [code])
+        .then(function (result) {
+            console.log('Module deleted successfully');
+        })
+        .catch(function (error) {
+            throw error;
+    });
+}
 
 // module.exports.updateByCode = function updateByCode(code, credit) {
 //     // Note:
